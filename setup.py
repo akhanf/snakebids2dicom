b8007bd6c3364ddea2260a4d3261e5a7e1138cb4
akhanf <alik@robarts.ca>
2021-05-06 15:30:54 -0400

    bugfixes and added features

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['snakebids==0.3.0',
+requirements = ['snakebids==0.3.9',
                 'pydicom' ]
 
 setup_requirements = [ ]
